d535938a70ad4e42b866dec852a670d5e3cb1b73
Aungkokolin1997 <aungkokolin23397@gmail.com>
2023-01-04 15:17:49 +0630

    [ADD] sale_report_qweb_decimal_place

---
+++ sale_report_qweb_decimal_place/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2023 Quartile Limited
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale Report Qweb Decimal Place",
+    "category": "Reporting",
+    "version": "16.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "license": "AGPL-3",
+    "depends": ["sale", "report_qweb_decimal_place"],
+    "data": [
+        "reports/sale_report.xml",
+    ],
+    "installable": True,
+}
---
+++ sale_report_qweb_decimal_place/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+This module provides the price unit format of sale report depends on the choice of price_decimal_places in currency.
