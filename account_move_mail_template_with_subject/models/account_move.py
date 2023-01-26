2916eef63be7260795d5135daee4e83b1a708346
Rinaldi Firdaus <rinaldi@quartile.co>
2023-01-26 04:19:49 +0000

    [3132][IMP] account_move_mail_template_with_subject: change record name

--- account_move_mail_template_with_subject/models/account_move.py
+++ account_move_mail_template_with_subject/models/account_move.py
@@ -9,9 +9,9 @@ class AccountMove(models.Model):
         :return: the correct mail template based on the current move type
         """
         return (
-            "account_move_mail_template_with_subject.email_template_edi_credit_note_with_subject"
+            "account_move_mail_template_with_subject.email_template_credit_note"
             if all(move.move_type == "out_refund" for move in self)
-            else "account_move_mail_template_with_subject.email_template_send_invoice_with_subject"
+            else "account_move_mail_template_with_subject.email_template_send_invoice"
         )
 
     subject = fields.Char(tracking=True)
