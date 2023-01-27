055e27e9957888bb8e8d5d21f7597c69f88e35fd
Rinaldi Firdaus <rinaldi@quartile.co>
2023-01-27 10:35:59 +0000

    [3132][ADD] account_move_subject

---
+++ account_move_subject/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2023 Quartile Limited
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+{
+    "name": "Account Move Subject",
+    "version": "16.0.1.0.0",
+    "category": "Invoices & Payments",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "depends": ["account"],
+    "license": "LGPL-3",
+    "data": [
+        "views/account_move_views.xml",
+    ],
+    "installable": True,
+}
