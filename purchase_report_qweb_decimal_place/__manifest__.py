de007afb2e2bf92117148015402791e6f48e198b
Aung Ko Ko Lin (Quartile) <45355704+AungKoKoLin1997@users.noreply.github.com>
2023-01-28 22:16:42 +0630

    [3006][ADD] purchase_report_qweb_decimal_place (#8)

---
+++ purchase_report_qweb_decimal_place/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2022 Quartile Limited
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Purchase Report Qweb Decimal Place",
+    "category": "Reporting",
+    "version": "16.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "license": "AGPL-3",
+    "depends": ["purchase", "report_qweb_decimal_place"],
+    "data": [
+        "reports/purchase_report.xml",
+    ],
+    "installable": True,
+}
