26a42cde141289b3a74f2ee484f02f9c1e88745e
Aung Ko Ko Lin (Quartile) <45355704+AungKoKoLin1997@users.noreply.github.com>
2023-01-28 22:15:14 +0630

    [3006][ADD] account_report_qweb_decimal_place (#7)

---
+++ account_report_qweb_decimal_place/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2023 Quartile Limited
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Account Report Qweb Decimal Place",
+    "category": "Reporting",
+    "version": "16.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "license": "AGPL-3",
+    "depends": ["account", "report_qweb_decimal_place"],
+    "data": [
+        "reports/invoice_report.xml",
+    ],
+    "installable": True,
+}
