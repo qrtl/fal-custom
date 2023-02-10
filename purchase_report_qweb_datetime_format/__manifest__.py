f9ac972f925bada2248284d4715e687e3fe7bd7d
Rinaldi Firdaus <rinaldi@quartile.co>
2023-02-10 12:12:35 +0000

    [3006][ADD] purchase_report_qweb_datetime_format

---
+++ purchase_report_qweb_datetime_format/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2023 Quartile Limited
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Purchase Report Qweb Datetime Format",
+    "category": "Reporting",
+    "version": "16.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "license": "AGPL-3",
+    "depends": ["purchase", "purchase_stock"],
+    "data": [
+        "reports/purchase_order_templates.xml",
+        "reports/purchase_quotation_templates.xml",
+    ],
+    "installable": True,
+}
