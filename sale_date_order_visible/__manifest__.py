e43613d92ad3eb05ccc74d587f5555ba234accbb
Aungkokolin1997 <lin@quartile.co>
2024-06-26 08:11:21 +0000

    [ADD] sale_date_order_visible

---
+++ sale_date_order_visible/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2024 Quartile (https://www.quartile.co)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale Date Order Visible",
+    "category": "Sale",
+    "version": "16.0.1.0.0",
+    "author": "Quartile",
+    "website": "https://www.quartile.co",
+    "license": "AGPL-3",
+    "depends": ["sale"],
+    "data": [
+        "views/sale_order_views.xml",
+    ],
+    "installable": True,
+}
