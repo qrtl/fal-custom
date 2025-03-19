c0a2ccf0e1252f9626a20db482a15bbbad865e0d
Aungkokolin1997 <lin@quartile.co>
2025-03-19 04:29:27 +0000

    upd

--- account_payment_no_exchange_diff/__manifest__.py
+++ account_payment_no_exchange_diff/__manifest__.py
@@ -1,7 +1,7 @@
 # Copyright 2025 Quartile (https://www.quartile.co)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    "name": "Account Move Create Payment Entry",
+    "name": "Account Payment No Exchange Diff",
     "version": "16.0.1.0.0",
     "category": "Accounting",
     "author": "Quartile",
